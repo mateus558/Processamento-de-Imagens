3743c3c306ce91e06865b948122593e457584407
kevynswhants <kevynswhants@hotmail.com>
2018-04-14 09:14:55 -0300

    Correction Gamma

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-import os
+import os, sys
 from sys import platform
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 from PIL import Image
 
+pathname = os.path.dirname(sys.argv[0])
 
 def clear():
     if platform == "linux" or platform == "linux2":
@@ -13,10 +14,15 @@ def clear():
         os.system('clear')
     elif platform == "win32":
         os.system('cls')
-    return
 
-"""def put_alpha_channel(img):
-    img.putalpha(1)"""
+"""
+def put_alpha_channel(img):
+    img.putalpha(1)
+    
+print ('sys.argv[0] =', sys.argv[0])
+print ('path =', pathname)
+print ('full path =', os.path.abspath(pathname))
+"""
 
 
 def is_grey_scale(image):
@@ -38,20 +44,25 @@ def conversor_image(img):
 
 def save_image(img):
     if(img.format == 'JPG'):
-        img.save('out.jpg')
+        img.save('out','jpg')
     else:
-        img.save('out.png')
+        img.save('out','png')
 
 
 def squared_error(img1_np, img2_np):
     width = np.size(img1_np,1)
-    height = np.size(img1_np,2)
+    height = np.size(img1_np,0)
 
-    mean = 0
-    for i in range(width):
-        for j in range(height):
+    mean = 0.0
+    for i in range(height):
+        for j in range(width):
             for k in range(0,3):
                 mean += (img1_np[i][j][k] - img2_np[i][j][k]) ** 2
+
+    #for i1 in img1_np and i2 in img2_np:
+     #   for j1 in i1 and j2 in i2:
+      #      for k1 in j1 and k2 in j2:
+       #         mean += (k1 - k2) ** 2
 
     return mean
 
@@ -70,60 +81,53 @@ def mean_square_error(img1, img2):
 
 
 def signal_to_noise_ration(original_img, noisy_img):
-    width, height = original_img.size
-    width2, height2 = noisy_img.size
+    img1 = Image.open(os.path.join(pathname, original_img))
+    img2 = Image.open(os.path.join(pathname, noisy_img))
+
+    original_img_np = np.asarray(img1, dtype = np.uint8)
+    noisy_img_np = np.asarray(img2, dtype = np.uint8)
+
+    height = np.size(original_img_np, 0)
+    width = np.size(original_img_np, 1)
+
+    height2 = np.size(noisy_img_np, 0)
+    width2 = np.size(noisy_img_np, 1)
 
     if width == width2 and height == height2:
-        original_img_np = np.asarray(original_img, dtype = np.uint8)
-        noisy_img_np = np.asarray(noisy_img, dtype = np.uint8)
+        signal = 0.0
+        #for i in range(height):
+         #   for j in range(width):
+          #      for k in range(0,3):
+           #         signal += (noisy_img_np[i][j][k]) ** 2
 
-        signal = 0
-        for i in range(width):
-            for j in range(height):
-                for k in range(0,3):
-                    signal += (noisy_img_np[i][j][k]) ** 2
+        for i in noisy_img_np:
+            for j in i:
+                for k in j:
+                    signal += k ** 2
 
         return signal / squared_error(original_img_np, noisy_img_np)
     else:
         return 0
 
 
-def image_menu():
-    clear()
-    img = Image
-    while True:
-        print("1 - Open Image")
-        print("2 - Save Image")
-        print("3 - Show Image")
+def correction_gamma(image, gamma):
+    img = Image.open(os.path.join(pathname, image))
+    img_np = np.asarray(img, dtype = np.uint8)
 
-        o = int(input(">"))
-        clear()
-        if o == 1:
-            # path = input("Image path: ")
-            img = Image.open('C:/Users/kevyn/Desktop/Trabalho-1-de-processamento-de-Imagens/teste.jpg')
-            #img.show()
-            # img = np.asarray(img2, dtype=np.uint8)
-        elif o == 2:
-            #img_pil = Image.fromarray(img.astype(np.uint8))
-            save_image(img)
-        elif o == 3:
-            # implt = plt.imshow(img)
-            # plt.show()
-            img.show()
-        elif o == 4:
-            img1 = Image.open('C:/Users/kevyn/Desktop/Trabalho-1-de-processamento-de-Imagens/teste1.jpg')
-            img2 = Image.open('C:/Users/kevyn/Desktop/Trabalho-1-de-processamento-de-Imagens/teste2.jpg')
-            print(mean_square_error(img1, img2))
+    gamma_correction = 1 / gamma
+
+    height = np.size(img_np, 0)
+    width = np.size(img_np, 1)
+
+    for i in range(height):
+        for j in range(width):
+            new_r = int( 255 * (img_np[i][j][0] / 255) ** gamma_correction )
+            new_g = int( 255 * (img_np[i][j][1] / 255) ** gamma_correction )
+            new_b = int( 255 * (img_np[i][j][2] / 255) ** gamma_correction )
+            img.putpixel((j,i),(new_r, new_g, new_b))
+
+    img.save(os.path.join(pathname, ('correction_gamma_('+str(gamma)+')_'+image)))
+    img.show()
 
 
-def main():
-    clear()
-
-    while True:
-        print("1 - Image")
-        o = int(input(">"))
-        if o == 1:
-            image_menu()
-
-
-main()
+signal_to_noise_ration('teste.jpg', 'correction_gamma_(5.0)_teste.jpg')
