ab66c0de080ee5d8f5d08a879650091962c46b98
kevynswhants <kevynswhants@hotmail.com>
2018-04-14 19:19:26 -0300

    MSE and SNR

--- main.py
+++ main.py
@@ -1,88 +1,74 @@
 import os, sys
 from sys import platform
 import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
+#import matplotlib.pyplot as plt
+#import matplotlib.image as mpimg
 from PIL import Image
 
 pathname = os.path.dirname(sys.argv[0])
 
-def clear():
-    if platform == "linux" or platform == "linux2":
-        os.system('clear')
-    elif platform == "darwin":
-        os.system('clear')
-    elif platform == "win32":
-        os.system('cls')
+# conversor_image('lena.jpg', 1)
+def conversor_image(img_name, channels):
+    img = Image.open(os.path.join(pathname, img_name))
 
-"""
-def put_alpha_channel(img):
-    img.putalpha(1)
-    
-print ('sys.argv[0] =', sys.argv[0])
-print ('path =', pathname)
-print ('full path =', os.path.abspath(pathname))
-"""
-
-
-def is_grey_scale(image):
-    img = image.convert('RGB')
-    width, height = img.size
-    for i in range(width):
-        for j in range(height):
-            r, g, b = img.getpixel((i,j))
-            if r != g != b: return False
-    return True
-
-
-def conversor_image(img):
-    if(is_grey_scale(img)):
+    if channels == 1:
+        return img.convert('L')
+    elif channels == 2:
+        return img.convert('LA')
+    elif channels == 2:
         return img.convert('RGB')
     else:
-        return img.convert('L')
+        return img.convert('RGBA')
 
 
-def save_image(img):
-    if(img.format == 'JPG'):
-        img.save('out','jpg')
-    else:
-        img.save('out','png')
+# save_image(img, 'lena.jpg')
+def save_image(img, img_name):
+    img.save(os.path.join(pathname, (img_name)))
 
-
-def squared_error(img1_np, img2_np):
-    width = np.size(img1_np,1)
+# squared_error(img_np, img2_np, 'TC')
+def squared_error(img1_np, img2_np, format):
     height = np.size(img1_np,0)
+    width = np.size(img1_np,1)
 
-    mean = 0.0
-    for i in range(height):
-        for j in range(width):
-            for k in range(0,3):
-                mean += (img1_np[i][j][k] - img2_np[i][j][k]) ** 2
+    error = 0.0
+    for i in range (height):
+        for j in range (width):
+            if format == 'TC':
+                error += ((img1_np[i][j] - img2_np[i][j]) ** 2)
+            if format == 'RGB':
+                for k in range (0,3):
+                    img1_aux = np.int(img1_np[i][j][k])
+                    img2_aux = np.int(img2_np[i][j][k])
+                    error += ((img1_aux - img2_aux) ** 2)
 
-    #for i1 in img1_np and i2 in img2_np:
-     #   for j1 in i1 and j2 in i2:
-      #      for k1 in j1 and k2 in j2:
-       #         mean += (k1 - k2) ** 2
+    return error
 
-    return mean
+# mean_square_error(img1_np, img2_np, 'TC')
+def mean_square_error(img1_name, img2_name, format):
+    img1 = Image.open(os.path.join(pathname, img1_name))
+    img2 = Image.open(os.path.join(pathname, img2_name))
 
+    img1_np = np.asarray(img1, dtype = np.uint8)
+    img2_np = np.asarray(img2, dtype = np.uint8)
 
-def mean_square_error(img1, img2):
-    width, height = img1.size
-    width2, height2 = img2.size
+    height = np.size(img1_np, 0)
+    width = np.size(img1_np, 1)
+
+    height2 = np.size(img2_np, 0)
+    width2 = np.size(img2_np, 1)
 
     if width == width2 and height == height2:
-        img1_np = np.asarray(img1, dtype = np.uint8)
-        img2_np = np.asarray(img2, dtype = np.uint8)
-
-        return squared_error(img1_np, img2_np) / (width * height)
+        if format == 'TC':
+            return squared_error(img1_np, img2_np, format) / (width * height)
+        elif format == 'RGB':
+            return squared_error(img1_np, img2_np, format) / (width * height * 3)
     else:
         return 0
 
-
-def signal_to_noise_ration(original_img, noisy_img):
-    img1 = Image.open(os.path.join(pathname, original_img))
-    img2 = Image.open(os.path.join(pathname, noisy_img))
+# signal_to_noise_ration('lena.jpg', 'lena2.jpg', 'RBG')
+def signal_to_noise_ration(original_img_name, noisy_img_name, format):
+    img1 = Image.open(os.path.join(pathname, original_img_name))
+    img2 = Image.open(os.path.join(pathname, noisy_img_name))
 
     original_img_np = np.asarray(img1, dtype = np.uint8)
     noisy_img_np = np.asarray(img2, dtype = np.uint8)
@@ -95,22 +81,20 @@ def signal_to_noise_ration(original_img, noisy_img):
 
     if width == width2 and height == height2:
         signal = 0.0
-        #for i in range(height):
-         #   for j in range(width):
-          #      for k in range(0,3):
-           #         signal += (noisy_img_np[i][j][k]) ** 2
+        for i in range(height):
+            for j in range(width):
+                if format == 'TC':
+                    signal += (noisy_img_np[i][j]) ** 2
+                if format == 'RGB':
+                    for k in range (0,3):
+                        signal += (noisy_img_np[i][j][k]) ** 2
 
-        for i in noisy_img_np:
-            for j in i:
-                for k in j:
-                    signal += k ** 2
-
-        return signal / squared_error(original_img_np, noisy_img_np)
+        return ( signal / squared_error(original_img_np, noisy_img_np, format) )
     else:
         return 0
 
-
-def correction_gamma(image, gamma):
+# correction_gamma('lena.jpg', 0.5, 'RBG')
+def correction_gamma(image, gamma, format):
     img = Image.open(os.path.join(pathname, image))
     img_np = np.asarray(img, dtype = np.uint8)
 
@@ -121,13 +105,62 @@ def correction_gamma(image, gamma):
 
     for i in range(height):
         for j in range(width):
-            new_r = int( 255 * (img_np[i][j][0] / 255) ** gamma_correction )
-            new_g = int( 255 * (img_np[i][j][1] / 255) ** gamma_correction )
-            new_b = int( 255 * (img_np[i][j][2] / 255) ** gamma_correction )
-            img.putpixel((j,i),(new_r, new_g, new_b))
+            if format == 'TC':
+                new = int( 255 * (img_np[i][j] / 255) ** gamma_correction )
+                img.putpixel((j, i), (new))
+            elif format == 'RGB':
+                new_r = int( 255 * (img_np[i][j][0] / 255) ** gamma_correction )
+                new_g = int( 255 * (img_np[i][j][1] / 255) ** gamma_correction )
+                new_b = int( 255 * (img_np[i][j][2] / 255) ** gamma_correction )
+                img.putpixel((j,i),(new_r, new_g, new_b))
 
-    img.save(os.path.join(pathname, ('correction_gamma_('+str(gamma)+')_'+image)))
+    img.save(os.path.join(pathname, ('correction_gamma_'+str(gamma)+'_'+image)))
     img.show()
 
 
-signal_to_noise_ration('teste.jpg', 'correction_gamma_(5.0)_teste.jpg')
+"""
+def clear():
+    if platform == "linux" or platform == "linux2":
+        os.system('clear')
+    elif platform == "darwin":
+        os.system('clear')
+    elif platform == "win32":
+        os.system('cls')
+
+def put_alpha_channel(img):
+    img.putalpha(1)
+    
+print ('sys.argv[0] =', sys.argv[0])
+print ('path =', pathname)
+print ('full path =', os.path.abspath(pathname))
+
+img = Image.open(os.path.join(pathname, 'lena.jpg'))
+img_np = np.asarray(img, dtype = np.uint8)
+
+img = conversor_image('lena.jpg', 1)
+save_image(img, 'lena_LA_converted.png')
+
+height = np.size(img_np, 0)
+width = np.size(img_np, 1)
+
+for i in range(height):
+    for j in range(width):
+        new_r = int( 0 )
+        new_g = int( 0 )
+        new_b = int( 0 )
+        img.putpixel((j,i),(new_r, new_g, new_b))
+
+img.save(os.path.join(pathname, ('preto.jpg')))
+
+for i in range(height):
+    for j in range(width):
+        new_r = int( 255 )
+        new_g = int( 255 )
+        new_b = int( 255 )
+        img.putpixel((j,i),(new_r, new_g, new_b))
+
+img.save(os.path.join(pathname, ('branco.jpg')))
+"""
+
+#correction_gamma('lena_LA_converted.png', 0.5, 'TC')
+#print ( signal_to_noise_ration('correction_gamma_0.5_lena.jpg', 'correction_gamma_0.5_lena.jpg', 'RGB') )
