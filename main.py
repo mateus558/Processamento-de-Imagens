aeeb2ba0c89ac5e1f322915eea13c8a5dfb77d0f
Mateus Coutinho Marim <mateus.marim@ice.ufjf.br>
2018-04-17 10:16:38 -0300

    Moost algorithms implemented.

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
 import os, sys
 from sys import platform
 import numpy as np
-#import matplotlib.pyplot as plt
+import math
+import matplotlib.pyplot as plt
 #import matplotlib.image as mpimg
 from PIL import Image
 
 pathname = os.path.dirname(sys.argv[0])
 
 # conversor_image('lena.jpg', 1)
-def conversor_image(img_name, channels):
+def open_image(img_name, channels):
     img = Image.open(os.path.join(pathname, img_name))
 
     if channels == 1:
         return img.convert('L')
     elif channels == 2:
         return img.convert('LA')
-    elif channels == 2:
+    elif channels == 3:
         return img.convert('RGB')
     else:
         return img.convert('RGBA')
@@ -24,6 +25,7 @@ def conversor_image(img_name, channels):
 # save_image(img, 'lena.jpg')
 def save_image(img, img_name):
     img.save(os.path.join(pathname, (img_name)))
+
 
 # squared_error(img_np, img2_np, 'TC')
 def squared_error(img1_np, img2_np, format):
@@ -42,6 +44,7 @@ def squared_error(img1_np, img2_np, format):
                     error += ((img1_aux - img2_aux) ** 2)
 
     return error
+
 
 # mean_square_error(img1_np, img2_np, 'TC')
 def mean_square_error(img1_name, img2_name, format):
@@ -64,6 +67,7 @@ def mean_square_error(img1_name, img2_name, format):
             return squared_error(img1_np, img2_np, format) / (width * height * 3)
     else:
         return 0
+
 
 # signal_to_noise_ration('lena.jpg', 'lena2.jpg', 'RBG')
 def signal_to_noise_ration(original_img_name, noisy_img_name, format):
@@ -93,6 +97,7 @@ def signal_to_noise_ration(original_img_name, noisy_img_name, format):
     else:
         return 0
 
+
 # correction_gamma('lena.jpg', 0.5, 'RBG')
 def correction_gamma(image, gamma, format):
     img = Image.open(os.path.join(pathname, image))
@@ -118,49 +123,174 @@ def correction_gamma(image, gamma, format):
     img.show()
 
 
-"""
-def clear():
-    if platform == "linux" or platform == "linux2":
-        os.system('clear')
-    elif platform == "darwin":
-        os.system('clear')
-    elif platform == "win32":
-        os.system('cls')
+def compute_histogram(image, bins, channels=1, plot=False):
+    interval_size = 256 / bins
+    if channels == 1:
+        flatten = image.ravel()
+        hist = np.zeros(shape=(bins, ), dtype=np.uint64)
 
-def put_alpha_channel(img):
-    img.putalpha(1)
-    
-print ('sys.argv[0] =', sys.argv[0])
-print ('path =', pathname)
-print ('full path =', os.path.abspath(pathname))
+        for i in range(0, len(flatten)):
+            hist[int(np.floor(flatten[i]/interval_size))] += 1
 
-img = Image.open(os.path.join(pathname, 'lena.jpg'))
-img_np = np.asarray(img, dtype = np.uint8)
+        if plot:
+            plt.bar(np.arange(bins)*interval_size, hist)
+            plt.show()
 
-img = conversor_image('lena.jpg', 1)
-save_image(img, 'lena_LA_converted.png')
+        return hist
+    else:
+        r = image[:, :, 0].ravel()
+        g = image[:, :, 1].ravel()
+        b = image[:, :, 2].ravel()
+        r_h = np.zeros(shape=(bins,), dtype=np.uint64)
+        g_h = np.zeros(shape=(bins,), dtype=np.uint64)
+        b_h = np.zeros(shape=(bins,), dtype=np.uint64)
+        for i in range(0, len(r)):
+            r_h[int(np.floor(r[i] / interval_size))] += 1
+            g_h[int(np.floor(g[i] / interval_size))] += 1
+            b_h[int(np.floor(b[i] / interval_size))] += 1
 
-height = np.size(img_np, 0)
-width = np.size(img_np, 1)
+        if plot:
+            a = plt.subplot(111)
+            a.plot(np.arange(bins)*interval_size, r_h, color='r')
+            a.plot(np.arange(bins)*interval_size, g_h, color='g')
+            a.plot(np.arange(bins)*interval_size, b_h, color='b')
+            plt.show()
+        return np.array([r_h, g_h, b_h])
 
-for i in range(height):
-    for j in range(width):
-        new_r = int( 0 )
-        new_g = int( 0 )
-        new_b = int( 0 )
-        img.putpixel((j,i),(new_r, new_g, new_b))
+def equalize_histogram(image, channels=1):
+    size = image.shape[0] * image.shape[1]
 
-img.save(os.path.join(pathname, ('preto.jpg')))
+    if channels == 1:
+        hist = compute_histogram(image, 256, 1)
+        pmf = hist/size #probability mass function
+        cdf = pmf   #cumulative distributive function
 
-for i in range(height):
-    for j in range(width):
-        new_r = int( 255 )
-        new_g = int( 255 )
-        new_b = int( 255 )
-        img.putpixel((j,i),(new_r, new_g, new_b))
+        for i in range(1, len(cdf)):
+            cdf[i] = cdf[i-1] + cdf[i]
+        cdf = cdf * 255
+        image[:, :] = cdf[image[:, :]]
 
-img.save(os.path.join(pathname, ('branco.jpg')))
-"""
+        return image
+    elif channels == 3:
+        r_hist, g_hist, b_hist = compute_histogram(image, 256, 3, False)
+        hist = np.array([r_hist, g_hist, b_hist])
+        pmf = hist / size
+        cdf = pmf
 
-#correction_gamma('lena_LA_converted.png', 0.5, 'TC')
-#print ( signal_to_noise_ration('correction_gamma_0.5_lena.jpg', 'correction_gamma_0.5_lena.jpg', 'RGB') )
+        for j in range(0, cdf.shape[0]):
+            for i in range(1, len(cdf)):
+                cdf[j][i] = cdf[j][i-1] + cdf[j][i]
+        cdf = cdf * 255
+
+        for i in range(0, image.shape[2]):
+            for j in range(0, image.shape[0]):
+                for k in range(0, image.shape[1]):
+                    image[j, k, i] = cdf[i, image[j, k, i]]
+
+        return image
+
+def global_limiarization(image, threshold):
+    bin = np.zeros((image.shape[0], image.shape[1],), dtype=np.uint8)
+
+    for i in range(image.shape[0]):
+        for j in range(image.shape[1]):
+            if np.all(image[i, j] > threshold):
+                bin[i, j] = 255
+    return bin
+
+def otsu_thresholding(img):
+    m, n = img.shape
+    size = m*n
+    p = compute_histogram(img, 256, 1)/size
+    psum = np.cumsum(p)
+    mk = np.zeros((256,), dtype=np.float)
+    var = np.zeros((256,), dtype=np.float)
+
+    varg = 0
+    mg = 0
+    p1 = 0
+    p2 = 0
+    m1 = 0
+    m2 = 0
+
+    for i in range(len(psum)):
+        mg += i * p[i]
+        mk[i] = mg
+
+    for i in range(len(psum)):
+        p1 += p[i]
+        p2 = 1 - p1
+
+        if p1 != 0:
+            m1 = mk[i] / p1
+        if p2 != 0:
+            m2 = (mg - mk[i]) / p2
+        var[i] = p1*p2*(m1-m2)*(m1-m2)
+
+        varg += (i - mg) * (i - mg) * p[i]
+
+    kstar = np.argmax(var)
+    n = var[kstar] / varg
+    bin = global_limiarization(img, kstar)
+
+    return bin, kstar, n
+
+
+def popularity_algorithm(img, ncolors, channels):
+    colors = img.getcolors(img.size[0]*img.size[1])
+    most_pop = []
+
+    for c in range(ncolors):
+        maxcount = -np.inf
+        ind = None
+        for i in range(len(colors)):
+            if colors[i][0] > maxcount:
+                maxcount = colors[i][0]
+                ind = i
+        most_pop.append(colors[i][1])
+        colors.remove(colors[i])
+
+    print("Most frequent colors found.")
+    img = pil_to_np(img)
+    if channels == 1: m, n = img.shape
+    else: m, n, _ = img.shape
+
+    for i in range(m):
+        for j in range(n):
+            old_dist = np.inf
+            ind = None
+            for k in range(ncolors):
+                dist = np.linalg.norm(img[i, j] - most_pop[:][k])
+
+                if dist < old_dist:
+                    ind = k
+                    old_dist = dist
+            img[i, j] = most_pop[:][ind]
+    plt.imshow(img)
+    plt.show()
+    return img
+
+
+def np_to_pil(image):
+    return Image.fromarray(image)
+
+
+def pil_to_np(image):
+    img = np.asarray(image, dtype = np.uint8)
+    img.setflags(write=1)
+    return img
+
+
+img = open_image('lena.jpg', 3)
+popularity_algorithm(img, 60, 3)
+img = pil_to_np(img)
+
+#compute_histogram(img, 256, 1, True)
+#img = equalize_histogram(img, 1)
+#bin = global_limiarization(img, 90)
+#print(bin)
+#bin, _, _ = otsu_thresholding(img)
+#plt.imshow(bin, cmap='gray')
+#plt.show()
+img = np_to_pil(img)
+save_image(img, "eq_lena.jpg")
