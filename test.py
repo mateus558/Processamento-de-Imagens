1efe9a251a2560b48fb5db884c884415ab00ea6d
Mateus Coutinho Marim <mateus.marim@ice.ufjf.br>
2018-06-19 09:37:54 -0300

    Amostragem e reconstrução testados.

--- test.py
+++ test.py
@@ -15,9 +15,9 @@ channels = 3
 img = open_image(img_name_in, channels)
 
 img = pil_to_np(img)
-img_s = resize(img, .5, 3, type="pontual", kind="slinear")
-
-show_image_np(img_s, 3)
+for i in range(10):
+    img_s = resize(img, 1-i*0.1, 3, type="area", kind="slinear")
+    show_image_np(img_s, 3)
 #fourier_transform_scipy(img)
 
 #fourier_transform(img, 2, 20, 30)
